f76bfdff39ed101e70aec62b2e5465989143e26f
sakae <zz511344@gmail.com>
2024-03-26 21:57:35 +0800

    AuraPlayerController
    1.建立基礎含式
    2.check斷言 沒有 input就給他crash
    3.建立inputSubSystem 綁定輸入資訊
    4.滑鼠設定相關 希望都維持顯示
    5.載入增強行輸入 模組

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -5,7 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
-
+class UInputMappingContext;
 /**
  * 
  */
@@ -13,5 +13,13 @@ UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+public:
+	AAuraPlayerController();
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere,Category="Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+
 };
