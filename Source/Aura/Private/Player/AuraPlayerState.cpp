79ef584757c0f17b0ee1d367108f9c5931999cff
sakae <zz511344@gmail.com>
2024-03-27 01:47:31 +0800

    The PlayerState
    1.新增 AuraPlayerState  建構子
    2.新增網路更新頻率 100(通常是這個數字
    3.新增BP_AuraPlayerState  並將GameMode 的 playState 切換

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,10 @@
+// sakae's gas project
+
+
+#include "..\..\Public\Player\AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	//通常是 100
+	NetUpdateFrequency = 100.0f;
+}
---
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -0,0 +1,18 @@
+// sakae's gas project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	AAuraPlayerState();
+};
