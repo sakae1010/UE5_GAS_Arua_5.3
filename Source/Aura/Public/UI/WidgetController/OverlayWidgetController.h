7014b7109e7ddef9e4f86069b0ecdfc70ace42bc
sakae <zz511344@gmail.com>
2024-04-06 15:59:43 +0800

    UI Widget Data Table
    1.新增四個 meessage Tag
    2.新增 Datatable messagewidgetData
    3.OverlayWidgetController 新增FUIWidgetRow 基礎綁定
    4.BP_OverlapWidgetController 綁定 DT_MessagewidgetData

--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "UI/Widget/AuraUserWidget.h"
 #include "UI/WidgetController/AuraWidgetController.h"
 #include "OverlayWidgetController.generated.h"
 
@@ -14,6 +15,25 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChanged, float, NewMana);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FonmaxManaChanged, float, NewMaxMana);
 
 // DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChanged, float, NewMana);
+
+USTRUCT(BlueprintType)
+struct FUIWidgetRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI")
+	FGameplayTag MessageTag = FGameplayTag();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI")
+	FText Message = FText();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI")
+	TSubclassOf<UAuraUserWidget> MessageWidget;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI")
+	UTexture2D* Image = nullptr;
+}; 
+
 /**
  * 
  */
@@ -39,6 +59,9 @@ public:
 
 	
 protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Widget Data")
+	TObjectPtr<UDataTable> MessageWidgetDataTable;
+	
 	void HealthChanged(const FOnAttributeChangeData& Data) const;
 	void MaxHealthChanged(const FOnAttributeChangeData& Data) const;
 	void ManaChanged(const FOnAttributeChangeData& Data) const;
