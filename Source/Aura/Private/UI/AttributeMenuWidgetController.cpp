15bc81f11e5d8a1e9ec03d8f3b05756c76c3d313
sakae <zz511344@gmail.com>
2024-04-20 22:15:24 +0800

    AttributeMenu Widget Controller
    1.建立AttributeMenuWidgetController

---
+++ Source/Aura/Private/UI/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+﻿// sakae's gas project
+
+
+#include "UI/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitValues()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BindCallBacksToDependencies()
+{
+	
+}
---
+++ Source/Aura/Public/UI/AttributeMenuWidgetController.h
@@ -0,0 +1,19 @@
+﻿// sakae's gas project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WidgetController/AuraWidgetController.h"
+#include "AttributeMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+public:
+	virtual void BroadcastInitValues() override;
+	virtual void BindCallBacksToDependencies() override;
+};
