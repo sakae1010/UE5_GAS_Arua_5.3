31792d41d0d6095d697e65f19cd3b9332b625a15
sakae <zz511344@gmail.com>
2024-04-25 00:52:23 +0800

    Aura Projectile
    1.建立 AuraProjectile
    2.建立 BP_FireBolt 來自 AuraProjectile 並且加入 火球特效
    3.地板重新鋪過

---
+++ Source/Aura/Public/Actor/AuraProjectile.h
@@ -0,0 +1,30 @@
+// sakae's gas project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AuraProjectile.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+UCLASS()
+class AURA_API AAuraProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AAuraProjectile();
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+
+protected:
+	virtual void BeginPlay() override;
+	UFUNCTION()
+	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	// UFUNCTION()
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+};
