ca77840fac359fee31a34dd4900e22d6bcc5bbb4
sakae <zz511344@gmail.com>
2024-04-25 01:15:11 +0800

    Aura Projectile Spell
    1.建立 AuraProjectileSpell
      覆寫 ActivateAbility PrintLog (測試順序
    2.建立 GA_FireBolt 實作 ActivateAbility  PringLog  並且綁定 InputTag.LMB
    3.BP_AuraCharacter 加入啟動abiltiy (test
    
    C++ 執行的會比 BP 快

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,14 @@
+﻿// sakae's gas project
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                           const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                           const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	UKismetSystemLibrary::PrintString(this, "ActivateAbility ( C++ )", true, false, FLinearColor::Red, 3.0f);
+}
---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -0,0 +1,18 @@
+﻿// sakae's gas project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraGameAbility.h"
+#include "AuraProjectileSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraProjectileSpell : public UAuraGameAbility
+{
+	GENERATED_BODY()
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
