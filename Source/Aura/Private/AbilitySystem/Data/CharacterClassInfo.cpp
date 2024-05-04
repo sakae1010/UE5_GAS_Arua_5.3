27f4604f3eef7d8ff930faa71792db1a6871818f
sakae <zz511344@gmail.com>
2024-05-04 21:12:43 +0800

    Character Class Info
    1. 建立CharacterClassInfo -> dataAsstet 格式
    2. 建立 enum ECharacterClass 分門別類 三種 元素師 戰士 弓箭手
    3. 建立 DA_CharacterClassInfo  (尚未塞入資料

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+﻿// sakae's gas project
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetCharacterClassInfo(ECharacterClass CharacterClass)
+{
+	return  CharacterClassInformation.FindChecked(CharacterClass);
+}
---
+++ Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,46 @@
+﻿// sakae's gas project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+class UGameplayAbility;
+
+UENUM(BlueprintType)
+enum class ECharacterClass : uint8
+{
+	Eleentalist,
+	Warrior,
+	Ranger,
+};
+
+USTRUCT()
+struct FCharacterClassDefaultInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, Category = "CharacterClass")
+	TSubclassOf<UGameplayAbility> PrimaryAttributes;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Character Class Defaults")
+	TMap<ECharacterClass,FCharacterClassDefaultInfo> CharacterClassInformation;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defaults")
+	TSubclassOf<UGameplayAbility> SeconderAttributes;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defaults")
+	TSubclassOf<UGameplayAbility> VitalAttributes;
+
+
+	FCharacterClassDefaultInfo GetCharacterClassInfo(ECharacterClass CharacterClass);
+};
