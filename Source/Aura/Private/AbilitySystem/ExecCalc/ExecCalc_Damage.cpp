7214b93ec56508c63785340eef5f0e587f2cf1c5
sakae <zz511344@gmail.com>
2024-05-07 22:21:36 +0800

    Damage Execution Calculation
    1. 建立ExecCalc_Damage 繼承 UGameplayEffectExecutionCalculation
    2. 覆寫Execute_Implementation 並且 取得 來源 與目標

---
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -0,0 +1,26 @@
+﻿// sakae's gas project
+
+
+#include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
+
+#include "AbilitySystemComponent.h"
+
+UExecCalc_Damage::UExecCalc_Damage()
+{
+	
+}
+
+void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+
+	const AActor* SourceActor = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
+	const AActor* TargetActor = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
+
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+	
+	
+	
+}
