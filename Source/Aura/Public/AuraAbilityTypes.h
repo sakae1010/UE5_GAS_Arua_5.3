5ee4270ba0eacf1c61751d4e8255e558838b3032
sakae <zz511344@gmail.com>
2024-05-10 00:45:45 +0800

    Struct Ops Type Traits
    1. 建立結構運算類型
    2. 繼承TStructOpsTypeTraitsBase2<T> T帶入 FAuraGameplayEffectContext
    3. 5.3版本對應修改

--- Source/Aura/Public/AuraAbilityTypes.h
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -21,8 +21,22 @@ public:
 	
 	virtual UScriptStruct* GetScriptStruct() const override
 	{
-		return FGameplayEffectContext::StaticStruct();
+		return StaticStruct();
 	}
+
+	/** Creates a copy of this context, used to duplicate for later modifications */
+	virtual FAuraGameplayEffectContext* Duplicate() const override
+	{
+		FAuraGameplayEffectContext* NewContext = new FAuraGameplayEffectContext();
+		*NewContext = *this;
+		if (GetHitResult())
+		{
+			// Does a deep copy of the hit result
+			NewContext->AddHitResult(*GetHitResult(), true);
+		}
+		return NewContext;
+	}
+	
 	virtual bool NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess) override;
 	
 protected:
@@ -30,4 +44,14 @@ protected:
 	bool bIsCriticalHit = false;
 	UPROPERTY()
 	bool bIsBlockedHit = false;
+};
+
+template<>
+struct TStructOpsTypeTraits<FAuraGameplayEffectContext> : public TStructOpsTypeTraitsBase2<FAuraGameplayEffectContext>
+{
+	enum
+	{
+		WithNetSerializer = true,
+		WithCopy = true,
+	};
 };
