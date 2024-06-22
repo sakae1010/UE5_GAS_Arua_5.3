8e15b1d3438b58dcb4f2d1f556eb45a1633949a7
sakae <zz511344@gmail.com>
2024-06-22 13:55:32 +0800

    [Enemy Finishing Touches] 219. Async Spawn Times
    1.BP_SommonAbility 新增 遞迴 等待 debug
    2.AuraSummonAbility 生成地點打射線確保生成點在地板
    
    Signed-off-by: sakae <zz511344@gmail.com>

--- Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
@@ -15,16 +15,20 @@ TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
 
 	const FVector LeftOfSpread = Forward.RotateAngleAxis(-SpawnSpread / 2.f, FVector::UpVector);
 	TArray<FVector> SpawnLocations;
+	const FVector LineTraceValue = FVector(0, 0, 100);
 	for (int32 i = 0; i < NumMinionsToSpawn; i++)
 	{
 		const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
-		const FVector ChosenSpawnLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
-		SpawnLocations.Add(ChosenSpawnLocation);
+		FVector ChosenSpawnLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
 
-		DrawDebugSphere(GetWorld(), ChosenSpawnLocation, 18.f, 12, FColor::Cyan, false, 3.f );
-		UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Location, Location + Direction * MaxSpawnDistance, 4.f, FLinearColor::Green, 3.f );
-		DrawDebugSphere(GetWorld(), Location + Direction * MinSpawnDistance, 5.f, 12, FColor::Red, false, 3.f );
-		DrawDebugSphere(GetWorld(), Location + Direction * MaxSpawnDistance, 5.f, 12, FColor::Red, false, 3.f );
+		FHitResult HitResult;
+		GetWorld()->LineTraceSingleByChannel(HitResult, ChosenSpawnLocation+LineTraceValue, ChosenSpawnLocation-LineTraceValue, ECC_Visibility);
+		if (HitResult.bBlockingHit)
+		{
+			ChosenSpawnLocation = HitResult.ImpactPoint;
+		}
+		
+		SpawnLocations.Add(ChosenSpawnLocation);
 	}
 	return SpawnLocations;
 }
