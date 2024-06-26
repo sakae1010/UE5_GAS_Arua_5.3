d53471eec9a85266c8450fcad0bac98d90ea0c4e
sakae <zz511344@gmail.com>
2024-06-26 22:47:29 +0800

    [Cost And Cooldown ] 244. Cooldown Async Task[]
    1. 新增 WaitCooldownChange 實作 通知系統 建立BP_Function 並且實作 OnCooldownStart 開觸發 以及OnCooldownEnd 觸發
    2. BP_SpellGlobes 實作 測試 WaitCooldownChange  是否正常觸發
    
    Signed-off-by: sakae <zz511344@gmail.com>

---
+++ Source/Aura/Private/AsyncTasks/WaitCooldownChange.cpp
@@ -0,0 +1,78 @@
+﻿// sakae's gas project
+
+
+#include "AsyncTasks/WaitCooldownChange.h"
+#include "AbilitySystemComponent.h"
+#include "GameplayEffect.h"
+#include "ActiveGameplayEffectHandle.h"
+
+UWaitCooldownChange* UWaitCooldownChange::WaitCooldownChange(UAbilitySystemComponent* AbilitySystemComponent,
+                                                             const FGameplayTag InCooldownTags)
+{
+	UWaitCooldownChange* WaitCooldownChange = NewObject<UWaitCooldownChange>();
+	WaitCooldownChange->ASC = AbilitySystemComponent;
+	WaitCooldownChange->CooldownTag = InCooldownTags;
+
+	if (!IsValid( AbilitySystemComponent) || !InCooldownTags.IsValid())
+	{
+		WaitCooldownChange->EndTask();
+		return nullptr;
+	}
+	// To Know when a cooldown has Ended(cooldown tag has been Remove)
+	//知道冷卻已結束（冷卻標籤已被移除）
+	AbilitySystemComponent->RegisterGameplayTagEvent(
+		InCooldownTags,
+		EGameplayTagEventType::NewOrRemoved).AddUObject(
+			WaitCooldownChange,
+			&UWaitCooldownChange::CooldownTagChange	);
+	
+	// To Know when a cooldown effect has been applied
+	//知道冷卻效果已被應用
+	AbilitySystemComponent->OnActiveGameplayEffectAddedDelegateToSelf.AddUObject(WaitCooldownChange,&UWaitCooldownChange::OnActiveEffectAdded);
+	return WaitCooldownChange;
+}
+
+void UWaitCooldownChange::EndTask()
+{
+	if (!IsValid(ASC))return;
+	ASC->RegisterGameplayTagEvent(CooldownTag,EGameplayTagEventType::NewOrRemoved).RemoveAll(this);
+
+	SetReadyToDestroy();
+	//標記垃圾回收
+	MarkAsGarbage();
+}
+
+void UWaitCooldownChange::CooldownTagChange(const FGameplayTag InCooldownTag, int32 NewCount)
+{
+	if(NewCount <=0)
+	{
+		OnCooldownEnd.Broadcast(0.f);
+	}
+}
+
+void UWaitCooldownChange::OnActiveEffectAdded(UAbilitySystemComponent* TargetAsc,const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveHandle)
+{
+	FGameplayTagContainer AssetTags;
+	SpecApplied.GetAllAssetTags(AssetTags);
+	FGameplayTagContainer GrantedTags;
+	SpecApplied.GetAllGrantedTags(GrantedTags);
+	if(AssetTags.HasTag(CooldownTag) || GrantedTags.HasTagExact(CooldownTag))
+	{
+		FGameplayEffectQuery Query = FGameplayEffectQuery::MakeQuery_MatchAllOwningTags(CooldownTag.GetSingleTagContainer());
+		TArray<float> TimesRemaining = ASC->GetActiveEffectsTimeRemaining(Query);
+		if(TimesRemaining.Num()>0)
+		{
+			float TimeRemaining = TimesRemaining[0];
+			for(int32 i = 1; i<TimesRemaining.Num();i++)
+			{
+				if(TimesRemaining[i]>TimeRemaining)
+				{
+					TimeRemaining = TimesRemaining[i];
+				}
+			}
+			
+			OnCooldownStart.Broadcast(TimeRemaining);
+		}
+
+	}
+}
---
+++ Source/Aura/Public/AsyncTasks/WaitCooldownChange.h
@@ -0,0 +1,47 @@
+﻿// sakae's gas project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "GameplayTagContainer.h"
+#include "WaitCooldownChange.generated.h"
+
+struct FActiveGameplayEffectHandle;
+struct FGameplayEffectSpec;
+struct FGameplayTag;
+class UAbilitySystemComponent;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCooldownChangeSignature, float, TimeRemaining);
+
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UWaitCooldownChange : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+public:
+	// UPROPERTY(BlueprintAssignable)
+	// FCooldownChangeSignature OnCooldownChange;
+	UPROPERTY(BlueprintAssignable)
+	FCooldownChangeSignature OnCooldownStart;
+	UPROPERTY(BlueprintAssignable)
+	FCooldownChangeSignature OnCooldownEnd;
+
+	UFUNCTION(BlueprintCallable ,meta = (BlueprintInternalUseOnly = "true"))
+	static UWaitCooldownChange* WaitCooldownChange(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayTag InCooldownTags);
+	
+	UFUNCTION(BlueprintCallable)
+	void EndTask();
+
+protected:
+	UPROPERTY()	
+	TObjectPtr<UAbilitySystemComponent> ASC;
+
+	FGameplayTag CooldownTag;
+
+	void CooldownTagChange(const FGameplayTag InCooldownTag, int32 NewCount);
+
+	void OnActiveEffectAdded( UAbilitySystemComponent* TargetAsc, const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveHandle);
+};
