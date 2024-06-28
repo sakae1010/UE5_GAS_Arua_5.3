3932b9475e02fda1fa868c54d54b29572aaf33eb
sakae <zz511344@gmail.com>
2024-06-28 21:44:33 +0800

    [ Experience and Leveling Up ] 248. Experience and Leveling Up(架構解說)  & 249. Level Up Info Data Asset
    1.新增 ULevelUpInfo : UDataAsset 建立 FAuraLevelUpInfo struct
    2.ULevelUpInfo 實作FindLevelForXP
    3.建立DA_LevelUpInfo 建立 到10等素質
    
    Signed-off-by: sakae <zz511344@gmail.com>

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,31 @@
+﻿// sakae's gas project
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+
+int32 ULevelUpInfo::FindLevelForXP(int32 InXP) const
+{
+	int32 Level = 1;
+	bool bSearching = false;
+	while (bSearching)
+	{
+		if (LevelUpInfos.Num()-1 <= Level)
+		{
+			break;
+		}
+
+		if (InXP >= LevelUpInfos[Level].Exp)
+		{
+			++Level;
+		}
+		else
+		{
+			bSearching = false;
+		}
+	}		
+	
+
+
+	return Level;
+}
+
+
---
+++ Source/Aura/Public/AbilitySystem/Data/LevelUpInfo.h
@@ -0,0 +1,37 @@
+﻿// sakae's gas project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelUpInfo.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FAuraLevelUpInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Level Award")
+	int32 Exp;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Level Award")
+	int32 AttributePointAward = 1;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Level Award")
+	int32 SpellPointAward = 1;
+};
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ULevelUpInfo : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "LevelUpInfo")
+	TArray<FAuraLevelUpInfo> LevelUpInfos;
+
+	int32 FindLevelForXP(int32 InXP) const;
+	
+};
