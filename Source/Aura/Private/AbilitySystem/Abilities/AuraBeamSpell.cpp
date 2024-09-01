ce22a5e1082872e592737fe544c1aca602ca45d7
sakae <zz511344@gmail.com>
2024-09-01 20:43:05 +0800

    [ What a Shock ]328. First Trace Target
    1. AuraBeamSpell 建立尋找第一個目標 使用射線 複寫掉 滑鼠位置 以及人物
    2. GA_Electrocute 分別判定第一個目標 目標是否為敵人 或者 其他物件
    
    Signed-off-by: sakae <zz511344@gmail.com>

--- Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -4,6 +4,7 @@
 #include "AbilitySystem/Abilities/AuraBeamSpell.h"
 
 #include "GameFramework/Character.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 void UAuraBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult)
 {
@@ -22,4 +23,40 @@ void UAuraBeamSpell::StoreOwnerVariables()
 	if(!CurrentActorInfo)return;
 	OwnerPlayerController = Cast<APlayerController>(CurrentActorInfo->PlayerController.Get());
 	OwnerCharacter = Cast<ACharacter>(CurrentActorInfo->AvatarActor.Get());
+}
+
+void UAuraBeamSpell::TraceFirstTarget(const FVector& BeamTargetLocation)
+{
+	check( OwnerCharacter );
+	FHitResult HitResult = FHitResult();
+
+	if(OwnerCharacter->Implements<UCombatInterface>())
+	{
+		 if( USkeletalMeshComponent* Weapon = ICombatInterface::Execute_GetWeapon( OwnerCharacter ) )
+		 {
+		 	const FVector SocketLocation = Weapon->GetSocketLocation( FName( "TipSocket" ) );
+		    TArray<AActor*> ActorsToIgnore = TArray<AActor*>();
+		 	ActorsToIgnore.Add( OwnerCharacter );
+		 	
+		 	UKismetSystemLibrary::SphereTraceSingle(
+		 		OwnerCharacter,
+		 		SocketLocation,
+		 		BeamTargetLocation,
+		 		5.f,
+		 		ETraceTypeQuery::TraceTypeQuery1,
+		 		false,
+		 		ActorsToIgnore,
+		 		EDrawDebugTrace::None,
+		 		HitResult,
+		 		true
+		 	 );
+
+		 	if( HitResult.bBlockingHit )
+		 	{
+		 		MouseHitLocation = HitResult.ImpactPoint;
+		 		MouseHitActor = HitResult.GetActor();
+		 	}
+		 }
+	}
+
 }
