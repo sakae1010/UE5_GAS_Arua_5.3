c89ed869ef5bd7692f7932fffcd5e5e53c6b4938
sakae <zz511344@gmail.com>
2024-10-02 21:53:53 +0800

    [ Arcane Shards ]345. Magic Circle
    1.建立一個新的acter (MagicCircle) 帶有貼花(Decal) 物件
    
    Signed-off-by: sakae <zz511344@gmail.com>

---
+++ Source/Aura/Private/Actor/MagicCircle.cpp
@@ -0,0 +1,26 @@
+﻿// sakae's gas project
+
+
+#include "Actor/MagicCircle.h"
+#include "Components/DecalComponent.h"
+
+
+AMagicCircle::AMagicCircle()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	MagicCircleDecal = CreateDefaultSubobject<UDecalComponent>(TEXT("MagicCircleDecal"));
+	MagicCircleDecal->SetupAttachment(RootComponent);
+	
+}
+
+void AMagicCircle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMagicCircle::Tick(float DeltaTime)
+{
+	Super::Tick( DeltaTime );
+}
+
