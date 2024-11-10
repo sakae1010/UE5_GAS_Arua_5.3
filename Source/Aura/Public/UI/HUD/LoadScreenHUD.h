6902702669f2306ea2726a55c7db74ceebe58fe3
sakae <zz511344@gmail.com>
2024-11-11 03:54:19 +0800

    [ Saving Progress ]382. Constructing a View Model
    1.LoadScreenHUD 建立 LoadScreenWidgetClass /LoadScreenViewModel
    2.WBP_LoadScreen 系列 改繼承 WBP_LoadScreenWidget_Base
    3.新增BP_LoadScreen_ViewModel 並綁定至BP_LoadScreenHUD
    4.WBP_LoalMenu 更名 WBP_LoadScreen
    5.WBP_LoadScreenWidget_Base 新增FindLoadScreenViewModel 方法 並且實作取得
    
    Signed-off-by: sakae <zz511344@gmail.com>

--- Source/Aura/Public/UI/HUD/LoadScreenHUD.h
+++ Source/Aura/Public/UI/HUD/LoadScreenHUD.h
@@ -6,6 +6,8 @@
 #include "GameFramework/HUD.h"
 #include "LoadScreenHUD.generated.h"
 
+class UMVVM_LoadScreen;
+class ULoadScreenWidget;
 /**
  * 
  */
@@ -13,4 +15,21 @@ UCLASS()
 class AURA_API ALoadScreenHUD : public AHUD
 {
 	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "LoadScreenHUD")
+	TSubclassOf<UUserWidget> LoadScreenWidgetClass;
+
+	UPROPERTY(BlueprintReadOnly, Category = "LoadScreenHUD")
+	TObjectPtr<ULoadScreenWidget> LoadScreenWidget;
+
+	UPROPERTY(EditDefaultsOnly, Category = "LoadScreenHUD")
+	TSubclassOf<UMVVM_LoadScreen> LoadScreenViewModelClass;
+
+	UPROPERTY(BlueprintReadOnly, Category = "LoadScreenHUD")
+	TObjectPtr<UMVVM_LoadScreen> LoadScreenViewModel;
+protected:
+	virtual void BeginPlay() override;
+	// virtual void DrawHUD() override;
+	// virtual void Tick(float DeltaTime) override;
+	
 };
