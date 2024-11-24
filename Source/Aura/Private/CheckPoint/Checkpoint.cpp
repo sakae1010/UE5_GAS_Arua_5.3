2fa76dbb46d866beb844a491e9bc86a9d9871afc
sakae <zz511344@gmail.com>
2024-11-25 02:27:16 +0800

    [ Check Points ]398. Checkpoints
    1.建立 Checkpoint 事件與物件
    2.建立 BP_Checkpoint 事件綁定以及timeline
    3.Dungeon 放入 BP_Checkpoint
    4.MI_Checkpoint 開啟glow 參數
    
    Signed-off-by: sakae <zz511344@gmail.com>

---
+++ Source/Aura/Private/CheckPoint/Checkpoint.cpp
@@ -0,0 +1,52 @@
+﻿// sakae's gas project
+
+
+#include "CheckPoint/Checkpoint.h"
+
+#include "Components/SphereComponent.h"
+
+
+ACheckpoint::ACheckpoint(const FObjectInitializer& ObjectInitializer):Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	
+	CheckpointMesh = CreateDefaultSubobject<UStaticMeshComponent>("CheckpointMesh");
+	CheckpointMesh->SetupAttachment( GetRootComponent() );
+	CheckpointMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	Sphere->SetCollisionResponseToChannels(ECR_Block);
+	
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment( CheckpointMesh );
+	Sphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	Sphere->SetCollisionResponseToChannels(ECR_Ignore);
+	Sphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	
+}
+
+void ACheckpoint::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(OtherActor->ActorHasTag( FName( "Player" ) ))
+	{
+		HandleGlowEffect();
+	}
+}
+
+void ACheckpoint::BeginPlay()
+{
+	Super::BeginPlay();
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ACheckpoint::OnSphereOverlap);
+}
+
+void ACheckpoint::HandleGlowEffect()
+{
+	// 禁用球體組件的碰撞
+	Sphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	// 為檢查點網格創建動態材質實例
+	UMaterialInstanceDynamic* MaterialInstance = UMaterialInstanceDynamic::Create(CheckpointMesh->GetMaterial(0), this);
+	// 設置檢查點網格的動態材質實例
+	CheckpointMesh->SetMaterial(0, MaterialInstance);
+	// 觸發檢查點到達的藍圖事件
+	CheckpointReached(MaterialInstance);
+
+}
